3a8a7fe6f369c00c5374dac5329aa34891652e25
Alex <cyalan2019@gmail.com>
2024-03-07 15:53:17 +0500

    дз

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { UserList2 } from './UserList2';
 
+
 export type AddressType = {
   street: string;
   city: string;
@@ -10,11 +11,11 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: AddressType; // пропиши типизацию
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends: UserType[]; // пропиши типизацию
 };
 
 export const HW2 = () => {
@@ -45,12 +46,10 @@ export const HW2 = () => {
   }
 
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
-
   const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+    const filteredUsers = users.myFriends.filter(us => us.address.city === "Los Angeles" )
     setCurrentUsers({ myFriends: filteredUsers });
   };
-
   return (
     <div id={'hw02'}>
       <UserList2 users={currentUsers} filterUsers={filterUsers} />
